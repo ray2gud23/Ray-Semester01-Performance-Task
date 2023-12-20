57c448846d7c5e35c79011e057d641b6dbba3fda
Ray Earth Paul Omne <raomne@s.sfusd.edu>
2023-12-20 11:09:19 -0800

    Player Scene Complete

---
+++ player.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+@export var speed = 400
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		velocity.y += -1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x += -1
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+		
+	if velocity.length() > 0:
+		velocity.normalized() * speed
+		
+	position += velocity * delta
